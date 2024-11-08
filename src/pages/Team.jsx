1151bd90436e46f60b926c467fd3e72946e59255
Dawid Glapiak <dg18@live.it>
2024-11-08 16:39:12 +0100

    need to update team and create faq

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -107,7 +107,7 @@ const Team = () => {
       </div>
 
       {/* Blurred content */}
-      <div className="blur-[6px]">
+      <div className="blur-[3px]">
         <LampContainer>
           <motion.h2
             initial={{ opacity: 0.5, y: initialY }}
