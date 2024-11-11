e8a5b1a2711aefc41898d77a46ab2d83d85e5a2c
Dawid Glapiak <dg18@live.it>
2024-11-11 16:07:27 +0100

    updated reservation button

---
+++ src/components/ButtonPackageCard.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next"; // Import useTranslation
+
+const ButtonPackageCard = ({ styles }) => {
+  const { t } = useTranslation(); // Destructure the useTranslation hook
+
+  return (
+    <Link to="/contact">
+      <button
+        type="button"
+        className={`my-10 relative w-full px-4 mt-6 py-4 font-poppins font-medium text-[18px] text-white bg-black-gradient rounded-[10px] overflow-hidden group hover:text-zinc-200 transition-all duration-300`}
+      >
+        {/* Effetto Glow quando si passa sopra */}
+        <span className="absolute inset-0 bg-black opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+
+        {/* Contenuto del bottone */}
+        <span className="relative z-10">{t("button.reserve")}</span>
+
+        {/* Linea di bordi Glow */}
+        <span className="absolute inset-0 rounded-[10px] border-b-2 border-r-2 border-yellow-400 group-hover:border-yellow-500 transition-all duration-300"></span>
+      </button>
+    </Link>
+  );
+};
+
+export default ButtonPackageCard;
--- src/components/PackageCard.jsx
+++ src/components/PackageCard.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import styles from "../style";
 import { motion, useInView } from "framer-motion";
 import { useTranslation } from "react-i18next";
-import Button from "./Button";
+import ButtonPackageCard from "./ButtonPackageCard";
 
 const PackageCard = ({
   name,
@@ -37,7 +37,7 @@ const PackageCard = ({
       variants={cardVariants}
       initial="hidden"
       animate={isInView ? "show" : "hidden"}
-      className={`flex justify-between flex-col px-6 border-yellow-400 border-b border-r py-8 sm:px-8 sm:py-10 md:px-10 md:py-12 rounded-[20px] max-w-[100%] ${
+      className={`flex flex-col justify-between px-6 border-yellow-400 border-b border-r py-8 sm:px-8 sm:py-10 md:px-10 md:py-12 rounded-[20px] max-w-[100%] ${
         index === 1
           ? "custom-bg-card hover:feedback-card-hover"
           : "feedback-card"
@@ -45,52 +45,54 @@ const PackageCard = ({
       onMouseEnter={index === 1 ? undefined : onMouseEnter}
       onMouseLeave={index === 1 ? undefined : onMouseLeave}
     >
-      <div className="flex flex-row pt-6 sm:pt-8 md:pt-10">
-        <div className="flex flex-col ml-4">
-          <h4 className="font-poppins font-semibold text-[20px] leading-[32px] text-white text-gradient">
-            {t(name)}
-          </h4>
-          <p className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite ">
-            {t(title)}
-          </p>
-          <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white mt-6 sm:mt-8 md:mt-10">
-            {t(content)}
-          </p>
+      <div className="flex-grow">
+        <h4 className="font-poppins font-semibold text-[20px] leading-[32px] text-white text-gradient">
+          {t(name)}
+        </h4>
+        <p className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite ">
+          {t(title)}
+        </p>
+        <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white mt-6 sm:mt-8 md:mt-10">
+          {t(content)}
+        </p>
+        <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white">
+          {t(content2)}
+        </p>
+        {content2_5 && (
           <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white">
-            {t(content2)}
+            {t(content2_5)}
           </p>
-          {content2_5 && (
-            <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white">
-              {t(content2_5)}
-            </p>
-          )}
-          <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white">
-            {t(content3)}
+        )}
+        <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white">
+          {t(content3)}
+        </p>
+        <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-dimWhite mt-4">
+          {t(content4)}
+        </p>
+        {content5 && (
+          <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white mt-4">
+            {t(content5)}
           </p>
+        )}
+        {content6 && (
           <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-dimWhite mt-4">
-            {t(content4)}
+            {t(content6)}
           </p>
-          {content5 && (
-            <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white mt-4">
-              {t(content5)}
+        )}
+        <div className="flex flex-row items-center py-[6px] rounded-[10px] mb-2">
+          {bestDeal && (
+            <p className={`${styles.paragraph} mt-6`}>
+              <span className="text-black bg-blue-gradient pr-3 pl-3 rounded-xl">
+                {t(bestDeal)}
+              </span>
             </p>
           )}
-          {content6 && (
-            <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-dimWhite mt-4">
-              {t(content6)}
-            </p>
-          )}
-          <div className="flex flex-row items-center py-[6px] rounded-[10px] mb-2">
-            {bestDeal && (
-              <p className={`${styles.paragraph} mt-6`}>
-                <span className="text-black bg-blue-gradient pr-3 pl-3 rounded-xl">
-                  {t(bestDeal)}
-                </span>
-              </p>
-            )}
-          </div>
-          <Button />
         </div>
+      </div>
+
+      {/* Button aligned at the bottom */}
+      <div className="mt-4">
+        <ButtonPackageCard />
       </div>
     </motion.div>
   );
