f86e105a111d9c9842636c92bd26b8cbbc9f4bfa
Dawid Glapiak <dg18@live.it>
2024-11-20 08:26:38 +0100

    reso sobrio il testo

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -31,7 +31,7 @@ const FAQItem = ({ question, answer, isOpen, toggle }) => (
 
 const FAQ = () => {
   const { t } = useTranslation();
-  const [openIndex, setOpenIndex] = useState(null);
+  const [openIndex, setOpenIndex] = useState(0); // La prima domanda sarà aperta di default
   const [showAll, setShowAll] = useState(false);
 
   const faqData = Array.from({ length: 18 }, (_, i) => ({
