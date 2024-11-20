e3790700502e4b802af890a26c7504ed85bd3788
Dawid Glapiak <dg18@live.it>
2024-11-20 10:32:34 +0100

    added playsinline in videos

--- src/components/Packages.jsx
+++ src/components/Packages.jsx
@@ -121,6 +121,7 @@ const Packages = () => {
             autoPlay
             loop
             muted
+            playsInline
           />
           <div className="absolute z-[3] w-[50%] h-[50%] left-1/2 transform -translate-x-1/2 top-0 rounded-full white__gradient" />
           <div className="absolute z-[0] w-[50%] h-[50%] left-1/2 transform -translate-x-1/2 bottom-0 rounded-full pink__gradient" />
